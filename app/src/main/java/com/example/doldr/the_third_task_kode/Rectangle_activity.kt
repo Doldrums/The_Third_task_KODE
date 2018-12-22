b640606ac087202ee2c0c7e75fe3bfb4c5a343d7
Doldrums <doldrums.317@gmail.com>
2018-12-22 22:50:45 +0200

    dfsfdsfffs

--- app/src/main/java/com/example/doldr/the_third_task_kode/Rectangle_activity.kt
+++ app/src/main/java/com/example/doldr/the_third_task_kode/Rectangle_activity.kt
@@ -2,11 +2,30 @@ package com.example.doldr.the_third_task_kode
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_rectangle.*
 
 class Rectangle_activity : AppCompatActivity() {
+    private var A : Double = 0.0
+    private var B : Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rectangle)
+
+        val rectangle = Rectangle()
+
+        btn_P.setOnClickListener {
+            A = txt_A.text.toString().toDouble()
+            B = txt_B.text.toString().toDouble()
+            txt_Result.text = rectangle.P(A,B).toString()
+
+
+        }
+        btn_S.setOnClickListener {
+            A = txt_A.text.toString().toDouble()
+            B = txt_B.text.toString().toDouble()
+            txt_Result.text = rectangle.S(A,B).toString()
+
+        }
     }
 }
